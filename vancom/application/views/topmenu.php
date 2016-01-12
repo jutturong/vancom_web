717ee4bbb82433bac24cce3978e510fd1e81c231
jutturong <jutturongcharoenrit@gmail.com>
2016-01-12 23:15:19 +0700

    12-1-59 3

--- vancom/application/views/topmenu.php
+++ vancom/application/views/topmenu.php
@@ -2,7 +2,10 @@
     <div class="easyui-panel" style="padding:5px;">
         <a href="#" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-large-clipart'  "><?=$head?></a>
          <a href="#" class="easyui-linkbutton" iconCls="icon-large-picture">ผู้ป่วยทั้งหมด/ประวัติการรักษา</a>
-        <a href="#" class="easyui-menubutton" data-options="menu:'#mm1',iconCls:'icon-edit'">Tool</a>
+        <a href="#" class="easyui-menubutton" data-options="menu:'#mm1',iconCls:'icon-edit'">Tool (ข้อมูลต่างๆ)</a>
+        
+         <a href="#" class="easyui-menubutton" data-options="menu:'#mm5',iconCls:'icon-add'">Add ComboBox (เพิ่มข้อมูล)</a>
+        
         <a href="#" class="easyui-menubutton" data-options="menu:'#mm4',iconCls:'icon-large-picture'">Report</a>
         <a href="#" class="easyui-menubutton" data-options="menu:'#mm2',iconCls:'icon-help'">Help</a>
        <!-- <a href="#" class="easyui-menubutton" data-options="menu:'#mm3'">ออกจากระบบ</a> -->
@@ -10,12 +13,14 @@
     </div>
     
     
+ 
+
     <div id="mm1" style="width:150px;">
         <!--<div data-options="iconCls:'icon-add'">เพิ่มชื่อผู้ป่วย</div>-->
         <div data-options="iconCls:'icon-add'">Tool เกี่ยวกับผู้ป่วย</div>
         <div class="menu-sep"></div>
-        <div>เพิ่มประวัติผู้ป่วย</div>
-        <div>เพิ่มประวัติการรักษา</div>
+        <div onclick="op_addpatien()" data-options=" iconCls:'icon-man'   " >เพิ่มประวัติผู้ป่วย</div>
+        <div onclick="op_diagnosis()"  data-options=" iconCls:' icon-large-shapes'   ">เพิ่มประวัติการรักษา</div>
         
         <div class="menu-sep"></div>
         <div  >
@@ -31,6 +36,34 @@
         </div>
         <!--<div data-options="iconCls:'icon-remove'">Delete</div>
         <div>Select All</div>-->
+    </div>
+ 
+ 
+  <div id="mm5" style="width:150px;">
+        <!--<div data-options="iconCls:'icon-add'">เพิ่มชื่อผู้ป่วย</div>-->
+        <div data-options="iconCls:'icon-large-shapes'   ">เพิ่มข้อมูล ComboBox</div>
+        <div class="menu-sep"></div>
+        <div> - เพิ่ม Indication</div>
+
+        
+        <!--
+        <div>
+            <span>Toolbar</span>
+            <div>
+                <div>Address</div>
+                <div>Link</div>
+                <div>Navigation Toolbar</div>
+                <div>Bookmark Toolbar</div>
+                <div class="menu-sep"></div>
+                <div>New Toolbar...</div>
+            </div>
+        </div>
+        -->
+        
+        <!--
+        <div data-options="iconCls:'icon-remove'">Delete</div>
+        <div>Select All</div>
+        -->
     </div>
     
     
