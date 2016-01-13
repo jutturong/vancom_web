834aa401019b1f72de7eb27b10b1f0c93e0406b5
jutturong <jutturongcharoenrit@gmail.com>
2016-01-13 21:52:59 +0700

    13-1-59 complete 2

--- vancom/application/views/topmenu.php
+++ vancom/application/views/topmenu.php
@@ -7,10 +7,16 @@
     <div class="easyui-panel" style="padding:5px;">
         <!--<a href="#" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-large-clipart'  "><?=$head?></a>-->
          <a href="#" class="easyui-linkbutton" iconCls="icon-large-clipart">ผู้ป่วยทั้งหมด/ประวัติการรักษา</a>
-         <a href="#" class="easyui-linkbutton" iconCls="icon-large-shapes" onclick="op_diagnosis()">เพิ่มประวัติการรักษา</a>
-        <a href="#" class="easyui-menubutton" data-options="menu:'#mm1',iconCls:'icon-edit'">Tool (ข้อมูลต่างๆ)</a>
+         
+         <a href="#" class="easyui-menubutton"  data-options=" menu:'#mm7' ,iconCls:'icon-man'   "   >เกี่ยวกับผู้ป่วย</a>
+       
+         
+       <!--  <a href="#" class="easyui-linkbutton" iconCls="icon-large-shapes" onclick="op_diagnosis()">เพิ่มประวัติการรักษา</a> -->
+         
+         
+       <!--  <a href="#" class="easyui-menubutton" data-options="menu:'#mm1',iconCls:'icon-edit'">Tool (ข้อมูลต่างๆ)</a> -->
         
-         <a href="#" class="easyui-menubutton" data-options="menu:'#mm5',iconCls:'icon-add'">Add ComboBox (เพิ่มข้อมูล)</a>
+         <a href="#" class="easyui-menubutton" data-options="menu:'#mm5',iconCls:'icon-large-shapes'">Add ComboBox (เพิ่มข้อมูล)</a>
         
         <a href="#" class="easyui-menubutton" data-options="menu:'#mm4',iconCls:'icon-large-picture'">Report</a>
           <a href="#" class="easyui-menubutton" data-options="menu:'#mm6', iconCls:' icon-large-smartart '  ">Graph</a>
@@ -20,12 +26,41 @@
     </div>
     
     
- 
-
-    <div id="mm1" style="width:150px;">
+ <div id="mm7" style="width:150px;">
         <!--<div data-options="iconCls:'icon-add'">เพิ่มชื่อผู้ป่วย</div>-->
+       <!--<div data-options="iconCls:'icon-add'">Tool เกี่ยวกับผู้ป่วย</div> -->
+       
+        <div class="menu-sep"></div>
+        <div onclick="op_addpatien()" data-options=" iconCls:'icon-man'   " >เพิ่มประวัติผู้ป่วย</div>
+        <div class="menu-sep"></div>
+        <div onclick="op_diagnosis()"  data-options=" iconCls:' icon-large-shapes'   ">เพิ่มประวัติการรักษา</div>
+        
+        <!--
+        <div  >
+            <span>ลบ/แก้ไข</span>
+            <div>
+                <div>แก้ไข/ลบ ประวัติผู้ป่วย</div>
+                <div class="menu-sep"></div>
+                <div>แก้ไข/ลบ ประวัติการรักษา</div>
+              
+                
+                <div>New Toolbar...</div>
+            </div>
+        </div>
+        -->
+        
+        <!--<div data-options="iconCls:'icon-remove'">Delete</div>
+        <div>Select All</div>-->
+    </div>
+
+
+   <!--
+    <div id="mm1" style="width:150px;">
+      
         <div data-options="iconCls:'icon-add'">Tool เกี่ยวกับผู้ป่วย</div>
         <div class="menu-sep"></div>
+        
+      
         <div onclick="op_addpatien()" data-options=" iconCls:'icon-man'   " >เพิ่มประวัติผู้ป่วย</div>
         
         <div onclick="op_diagnosis()"  data-options=" iconCls:' icon-large-shapes'   ">เพิ่มประวัติการรักษา</div>
@@ -39,22 +74,24 @@
                 <div>แก้ไข/ลบ ประวัติการรักษา</div>
               
                 
-                <!--<div>New Toolbar...</div>-->
+                <div>New Toolbar...</div>
             </div>
         </div>
-        <!--<div data-options="iconCls:'icon-remove'">Delete</div>
-        <div>Select All</div>-->
+        <div data-options="iconCls:'icon-remove'">Delete</div>
+        <div>Select All</div>
     </div>
+  -->
+ 
  
  
   <div id="mm5" style="width:150px;">
         <!--<div data-options="iconCls:'icon-add'">เพิ่มชื่อผู้ป่วย</div>-->
-        <div data-options="iconCls:'icon-large-shapes'   ">เพิ่มข้อมูล ComboBox</div>
+        <div data-options="iconCls:'icon-large-shapes'   ">เพิ่มข้อมูล</div>
         <div class="menu-sep"></div>
-        <div> - เพิ่ม Indication</div>
-        <div> - เพิ่ม Reason for TDM</div>
-        <div> - เพิ่ม Drug level requested</div>
-        <div> - เพิ่ม Add Current Medications</div>
+        <div >Indication</div>
+        <div>Reason for TDM</div>
+        <div>Drug level requested</div>
+        <div>Add Current Medications</div>
         <!--
         <div>
             <span>Toolbar</span>
